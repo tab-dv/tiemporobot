69aaf03c6d7aedbbf154a8270d63ec86a215a324
Alonso Banda <113001530+Qtrocious@users.noreply.github.com>
2024-03-10 14:27:43 -0600

    Update Robot.java
    añadizacion buena del shasis

--- Timed god/src/main/java/frc/robot/Robot.java
+++ Timed god/src/main/java/frc/robot/Robot.java
@@ -4,12 +4,32 @@
 
 package frc.robot;
 
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class Robot extends TimedRobot {
 
+  WPI_TalonSRX RMtrFllw = new WPI_TalonSRX(1);
+  WPI_TalonSRX RMtrEnc = new WPI_TalonSRX(2);
+
+  WPI_TalonSRX LMtrFllw = new WPI_TalonSRX(3);
+  WPI_TalonSRX LMtrEnc = new WPI_TalonSRX(4);
+
+  DifferentialDrive Chasis = new DifferentialDrive(RMtrEnc, LMtrEnc);
+
+  Joystick Joy = new Joystick(0);
+
   @Override
-  public void robotInit() {}
+  public void robotInit() {
+    RMtrEnc.setInverted(false);
+    LMtrEnc.setInverted(true);
+
+    RMtrFllw.follow(RMtrEnc);
+    LMtrFllw.follow(LMtrEnc);
+  }
 
   @Override
   public void robotPeriodic() {}
@@ -25,8 +45,9 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-    
+    Chasis.arcadeDrive(Joy.getRawAxis(2) - Joy.getRawAxis(3), Joy.getRawAxis(0));
   }
+
 
   @Override
   public void disabledInit() {}
